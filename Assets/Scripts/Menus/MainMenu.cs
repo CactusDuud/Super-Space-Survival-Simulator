f6e767cd7f965000e3916e7c0af9038dbddead00
maleman10 <74637240+maleman10@users.noreply.github.com>
2022-02-02 14:11:32 -0800

    escape button brings up pause menu
    Added code into the pause menu to be able to have players click the escape button as another way of escaping the game.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,9 +1,3 @@
-//contributors: Esmeralda Juarez, Miguel Aleman
-/*This script is used in the main menu in order too allow the player too 
- * start game
- * quit game*/
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
@@ -11,7 +5,11 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    //contributors: Esmeralda Juarez, Miguel Aleman
+    /*This script is used in the main menu in order too allow the player too 
+     * start game
+     * quit game*/
+
     //this function loads the starting level scene so that the player can start playing
     //it also sends a message in the log too make sure it works
     public void StartButton()
--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -1,11 +1,3 @@
-//contributors: Esmeralda Juarez, Miguel Aleman
-/*This script is used in the pause menu in order too allow the player too 
- * pause their game
- * resume
- * return too main menu*/
-
- //side note: this script was also used in the Game Over Menu since it had the code needed (just didnt change the name of the script)
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
@@ -13,16 +5,47 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    //contributors: Esmeralda Juarez, Miguel Aleman
+    /*This script is used in the pause menu in order too allow the player too 
+     * pause their game
+     * resume
+     * return too main menu*/
+
+    // checks to see if game is paused which it is not when starting so is set to false.
+    public static bool GamePaused = false;
+
+    // allows us to out the pausemenu in this object to show to players
+    public GameObject PauseUI;
+
+    //This update function checks to see if the escape button has been pushed, will then bring up pause menu
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
     //this function resumes the game from its paused state
     public void Resume()
     {
+        PauseUI.SetActive(false);
         Time.timeScale = 1f;
+        GamePaused = false;
     }
 
     //this function pauses the game
     public void Pause() 
     {
+        PauseUI.SetActive(true);
         Time.timeScale = 0f;
+        GamePaused = true;
     }
 
     //this function resumes the game and then loads the main menu scene
