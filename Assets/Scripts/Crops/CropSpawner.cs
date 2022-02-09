1aca54c4a72bf0b9488ee362f702b75828ff0436
Esme-gif <62487373+Esme-gif@users.noreply.github.com>
2022-02-08 22:29:56 -0800

    Position crop spawn corrected
    Just finished with correcting where in the tile the crop grows. Still need too work if another plant is there already (and possibly that player only spawns in soil tile if that what we doing)

--- Assets/Scripts/Crops/CropSpawner.cs
+++ Assets/Scripts/Crops/CropSpawner.cs
@@ -10,19 +10,67 @@ public class CropSpawner : MonoBehaviour
 {
     //variables will help with instantiate prefabs and put them in the correct heirarchy
     [Header("Prefabs")]
-    public GameObject plant1;
-    public Transform cropParent;
+    [SerializeField] private GameObject _plant1;
+    [SerializeField] private Transform _cropParent;
+
+    [Header("Placement")]
+    [SerializeField] private Grid _gridLayout;
+    [SerializeField] private float elevation = -0.25f;
+
+    private Vector3 _placement;
+    private bool _onSoil = false;
+    
+
 
 
     //this function gets a string type that lets the function know which plant too instantiate
     public void CreatePlant(string plantType)
     {
-        if (plantType == "potato")
+        Vector3Int tileCellPos = _gridLayout.WorldToCell(transform.position);
+        Vector3 centerCell = _gridLayout.GetCellCenterWorld(tileCellPos);
+        centerCell = new Vector3(centerCell.x, centerCell.y+ elevation, centerCell.z);
+
+        Instantiate(_plant1, centerCell, Quaternion.identity, _cropParent);
+        //position of crops is now good too go
+
+        /*if (_onSoil)
         {
-            Debug.Log("potato enemy!!!");
-            Instantiate(plant1, transform.position, Quaternion.identity, cropParent);
-        }
+            _placement = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+            if (plantType == "potato")
+            {
+                Debug.Log("potato enemy!!!");
+                Instantiate(_plant1, _placement, Quaternion.identity, _cropParent);
+            }
+        }*/
 
 
     }
+    /*
+     *  
+        Vector3Int cellPosition = gridLayout.WorldToCell(transform.position);
+        transform.position = gridLayout.CellToWorld(cellPosition); 
+         var grid = _refMan.ground.GetComponentInParent<Grid>();
+            if (_refMan.ground.GetTile(grid.WorldToCell(transform.position)))
+            
+           
+
+            if(!_refMan.ground.GetTile(_refMan.ground.layoutGrid.WorldToCell(transform.position)))
+           
+
+    */
+    //this function will basically look at what the player collidered with and turn true/false if they collided with soil
+    /*void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (_onSoil)
+        {
+            _onSoil = false;
+        }
+        else
+        {
+            _onSoil = true;
+        }
+        
+    }*/
+
 }
