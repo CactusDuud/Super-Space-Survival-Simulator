b5399f24c6fbe8b07d650e22be7aea29247d9082
Cactuus <kingcactaurx@gmail.com>
2022-02-14 13:53:36 -0800

    FlipFacing has been replaced
    rip bozo

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
 {
     [Header("References")]
     Rigidbody2D _rigidbody;
+    [SerializeField] Transform _gfx;
 
     [Header("Attributes")]
     [SerializeField] public float speed = 4f;
@@ -21,5 +22,16 @@ public class Movement : MonoBehaviour
     public void Move(Vector2 direction)
     {
         _rigidbody.velocity = direction * speed * Time.deltaTime;
+        Flip();
+    }
+    
+    void Flip()
+    {
+        Vector2 _newScale = _gfx.localScale;
+        if (_rigidbody.velocity.x > 0)
+            _newScale.x = -Mathf.Abs(_gfx.localScale.x);
+        else if (_rigidbody.velocity.x < 0)
+            _newScale.x = Mathf.Abs(_gfx.localScale.x);
+        _gfx.localScale = _newScale;
     }
 }
