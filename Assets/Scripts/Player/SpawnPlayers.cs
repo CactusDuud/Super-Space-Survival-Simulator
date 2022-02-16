d66d01aeb93077fdad17d6908c805bee6ecfcedb
Cactuus <kingcactaurx@gmail.com>
2022-02-16 14:42:11 -0800

    There are now two local players
    CropSpawner.cs probably doesn't work anymore, but I can probably move it into the crop manager and everything will work out.

---
+++ Assets/Scripts/Player/SpawnPlayers.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] GameObject _pf_player1;
+    [SerializeField] GameObject _pf_player2;
+    
+    
+
+    void Start()
+    {
+        // Spawn player 1
+        var p1 = PlayerInput.Instantiate(_pf_player1, controlScheme: "Player1", pairWithDevice: Keyboard.current);
+
+        // Spawn player 2
+        var p2 = PlayerInput.Instantiate(_pf_player2, controlScheme: "Player2", pairWithDevice: Keyboard.current);
+    }
+}
