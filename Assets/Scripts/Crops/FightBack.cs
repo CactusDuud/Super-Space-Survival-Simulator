4c69351ae690b3d1cca69842d101c10f86e0fa60
Esme-gif <62487373+Esme-gif@users.noreply.github.com>
2022-03-04 21:35:51 -0800

    Fight Back script in testing phase
    I was able to get the fight back script working somehow. when plant is attacked and there is an ontrigger collision the plant will fight back/ poison one enemy until it dies or plant itself dies

--- Assets/Scripts/Crops/FightBack.cs
+++ Assets/Scripts/Crops/FightBack.cs
@@ -1,6 +1,7 @@
 //Contributor: Esmeralda Juarez
 
 /*
+ * this was made as a child but now is just a component
  * this script is a child from Growth script class for the plants
  *      with this script attached to a plant when plant is taking damage from an enemy it fights back
  *      this child only focuses on one enemy until they die then moves on too the next attacker
@@ -11,39 +12,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FightBack : Growth
+public class FightBack : MonoBehaviour
 {
     [SerializeField] private int attackDamage;
     private Health _currentHealth;
     private int _prevHealth;
     private bool _isAttacked;
+    private bool _isCooling;
 
     private GameObject _enemy;
 
 
     //on awake initiate variables
-    protected override void Awake()
+    void Awake()
     {
-        base.Awake();
         _isAttacked = false;
+        _isCooling = false;
         _currentHealth = GetComponent<Health>();
         _prevHealth = _currentHealth._health;
         _enemy = null;
     }
 
     //override fixedupdate too check if the health is going down or not
-    protected override void FixedUpdate()
+    void FixedUpdate()
     {
-        base.FixedUpdate();
 
         // if plant has 0 health it no longer fights back
-        if (_currentHealth._health > 0)
+        if (_currentHealth._health > 0) //&& _isHarvestable)
         {
             if (_currentHealth._health < _prevHealth) //being attacked so calling function too fight back
             {
                 _isAttacked = true;
                 StartCoroutine(updateHealth(1.0f));
-                poisionAttack();  /// currentely being called too fast -> need too cooldown on each attack (enemy taking really fast hits)
+                if (!_isCooling)
+                {
+                    _isCooling = true;
+                    StartCoroutine(poisionAttack(0.5f));  /// currentely being called too fast -> need too cooldown on each attack (enemy taking really fast hits)
+                }
 
             }
             else if (_currentHealth._health == _prevHealth) // not being attacked
@@ -56,16 +61,15 @@ public class FightBack : Growth
                 _prevHealth = _currentHealth._health;
             }
         }
-        
+
     }
 
     //checks if enemy has collidered with plant, plant is being attacked, and if there is currently no other enemy plant if focused on
-    void OnTriggerEnter2D(Collider2D other) 
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("EnemyTag") && _isAttacked && _enemy == null)
         {
             _enemy = other.gameObject;
-            Debug.Log("fight back plant being attacked");
         }
     }
 
@@ -75,12 +79,14 @@ public class FightBack : Growth
         _prevHealth = _currentHealth._health;
     }
 
-    void poisionAttack() // damages the targeted enemy        could cool down when attack made
+    IEnumerator poisionAttack(float waitTime) // damages the targeted enemy        could cool down when attack made
     {
         if (_enemy != null)
         {
             _enemy.GetComponent<Health>().Damage(attackDamage);
         }
+        yield return new WaitForSeconds(waitTime);
+        _isCooling = false;
     }
 
 }
