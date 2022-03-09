e658d1ed77a18e39d67e490006a1137876d06a3f
Cactuus <kingcactaurx@gmail.com>
2022-03-08 20:41:31 -0800

    Update CropDeath.cs
    Just code cleanup :D

--- Assets/Scripts/Crops/CropDeath.cs
+++ Assets/Scripts/Crops/CropDeath.cs
@@ -2,15 +2,8 @@
 
 //job of this script is to kill a plant the moment it is harvested
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CropDeath : Growth
 {
-
-    
-
     public override void Harvest()
     {
         //looking to see if plant is harvestable
@@ -19,17 +12,10 @@ public class CropDeath : Growth
             //changing it to remove plant instead of replanting
            if (_health.IsAlive())
            {
-                GameManager.GetInstance.AddProsperity(_prosperityValue);
-                Destroy(this.gameObject);
-            }
-           else Destroy(this.gameObject);
-
+               GameManager.GetInstance.AddProsperity(_prosperityValue);
+           }
+           
+           Destroy(this.gameObject);
         }
-
-
-
     }
-
-
-
 }
