59a6dad674439a0472c0e32853449a4d95282892
ecastrej <ecastrej@uci.edu>
2022-03-08 19:16:28 -0800

    graphics ui fix
    yup awesome dog in help menu and carrot got nerfed

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -34,7 +34,7 @@ public class PauseMenu : MonoBehaviour
         if (_instance != null && _instance != this) Destroy(this.gameObject); 
         else _instance = this;
 
-        GamePaused = false;
+        Resume();
 
     }
 
